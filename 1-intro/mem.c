c28e1797a5645b987c151f466979cbbfdc099c7c
label01 <2521413036@qq.com>
2023-08-02 18:44:29 -0700

    补完mem.c

--- 1-intro/mem.c
+++ 1-intro/mem.c
@@ -1,2 +1,25 @@
 #include "stdio.h"
+#include "unistd.h"
+#include "stdlib.h"
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    int *p = malloc(sizeof(int));
+    assert(p != NULL);
+    printf("(%d) address pointed to by p: %p\n", getpid(), p);
+    *p = 0;
+
+    while (1)
+    {
+        /* code */
+        Spin(1);
+        *p = *p + 1;
+        printf("(%d) p: %d\n", getpid(), *p);
+    }
+    
+    
+    return 0;
+}
 
