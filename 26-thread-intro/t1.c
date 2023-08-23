48a38ca92c6769b340e810c9ce5a536e5ba3a9ee
label01 <2521413036@qq.com>
2023-08-22 19:05:00 -0700

    临时保存t1.c

--- 26-thread-intro/t1.c
+++ 26-thread-intro/t1.c
@@ -1,1 +1,14 @@
 #include "stdio.h"
+#include "stdlib.h"
+#include "pthread.h"
+
+#include "common.h"
+#include "common_threads.h"
+
+int max;
+volatile int count = 0; //shared global variable
+
+void *mythread(void *arg){
+    char *letter = arg;
+    
+}
