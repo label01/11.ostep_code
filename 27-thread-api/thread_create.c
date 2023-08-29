7549806dfa0afa2892798e17bb68bc6fd028860f
label01 <2521413036@qq.com>
2023-08-29 00:20:08 -0700

    补充27章例程

--- 27-thread-api/thread_create.c
+++ 27-thread-api/thread_create.c
@@ -2,3 +2,31 @@
 #include "stdio.h"
 #include "pthread.h"
 
+
+typedef struct {
+    int a;
+    int b;
+} myarg_t;
+
+
+
+void *mythread(void *arg){
+    myarg_t *args = (myarg_t *) arg;
+    printf("%d %d\n", args->a, args->b);
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    /* code */
+    pthread_t p;
+    myarg_t args = {10, 20};
+
+    int rc = pthread_create(&p, NULL, mythread, &args);
+    assert(rc == 0);
+    (void) pthread_join(p, NULL);
+    printf("done\n");
+    return 0;
+}
+
+
